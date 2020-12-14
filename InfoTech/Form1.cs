52f8ef89184dcec7a73e19b79abeb8863a039d6b
Prog2 <Prog2@technologyinfo.com>
2020-12-14 11:28:16 -0300

    #1 Comienzo del proyecto - creacion de login basico.

---
+++ InfoTech/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfoTech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcept_Click(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            // Comparar y si es igual abrir ventana principal.
+            if ( User.UserApp.Equals(user) && User.PassUser.Equals(pass) )
+            {
+                this.Hide();
+                frmMain objFrm = new frmMain();
+                objFrm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Datos Incorrectos");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ InfoTech/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoTech
+{
+    class User
+    {
+        public static string UserApp = "Pablo";
+        public static string PassUser = "1";
+        public static int LevelUser = 1;
+    }
+}
